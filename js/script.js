8e1eda8d231aba1a79acfb8e49404c3f5f07bf4d
umaransari <umaransari4444r@gmail.com>
2023-11-24 15:02:59 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function visitGitHub() {
+    window.open("https://github.com/Umarroxx")
+  }
+
+  function visitLinkedIn() {
+    window.open("https://www.linkedin.com/in/mohd-umar-ansari-6a420425b/")
+  }
