7bb38f34d3796253b46c1a6f12ded20ab3b8fcb8
umaransari <umaransari4444r@gmail.com>
2023-11-24 16:22:57 +0530

    edit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function visitGitHub() {
     window.open("https://github.com/Umarroxx")
   }
 
-  function visitLinkedIn() {
-    window.open("https://www.linkedin.com/in/mohd-umar-ansari-6a420425b/")
-  }
+function visitLinkedIn() {
+  window.open("https://www.linkedin.com/in/mohd-umar-ansari-6a420425b/")
+}
+
+function visitInstagram(){
+  window.open("https://www.instagram.com/umar__roxx/")
+}
